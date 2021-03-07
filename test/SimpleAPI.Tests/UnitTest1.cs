1ca989169f3cb02b084f86805334144aa9d298c6
shadab <wasai.gimt@gmail.com>
2021-03-07 19:51:50 +0530

    Initial commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using SimpleAPI.Controllers;
+using Xunit;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Bhawesh Paliwal", returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+    }
+}
